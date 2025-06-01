09933796978794e6b284d0a5b94a51e2d860a0f3
mzet97 <matheus.zeitune.developer@gmail.com>
2025-06-01 18:27:37 -0300

    fix: Update client UDP IP addresses for consistency across scripts

--- run_30_cycles.sh
+++ run_30_cycles.sh
@@ -1,9 +1,9 @@
 for cycle in {1..30}; do
   echo "=== Iniciando ciclo $cycle de 30 ==="
 
-  ./client_udp 0.0.0.0 0.0.0.0 50000 1 &
+  ./client_udp 10.0.0.16 10.0.0.12 50000 1 &
 
-  ./client_udp 0.0.0.0 0.0.0.0 50000 2 &
+  ./client_udp 10.0.0.17 10.0.0.12 50000 2 &
 
   wait
 
--- run_30_ex_2_cycles.sh
+++ run_30_ex_2_cycles.sh
@@ -1,9 +1,9 @@
 for cycle in {1..2}; do
   echo "=== Iniciando ciclo $cycle de 2 ==="
 
-  ./client_udp_ramp 0.0.0.0 0.0.0.0 50000 1 &
+  ./client_udp_ramp 10.0.0.16 10.0.0.12 50000 1 &
 
-  ./client_udp_ramp 0.0.0.0 0.0.0.0 50000 2 &
+  ./client_udp_ramp 10.0.0.14 10.0.0.12 50000 2 &
 
   wait
 
